3dab33bbbe569db538d9bbb2f2057e4b21101aee
simsimjae <simsimjae@naver.com>
2019-08-21 16:59:39 +0900

    상세 페이지 개편

--- resources/js/common.js
+++ resources/js/common.js
@@ -15,20 +15,12 @@ $(function() {
     }
   });
 
-  $(".write__inparea").on(
-    "focusin",
-    ".write__inpcnt, .write__txtarea",
-    function() {
-      $(this).addClass("focus");
-    }
-  );
-  $(".write__inparea").on(
-    "focusout",
-    ".write__inpcnt, .write__txtarea",
-    function() {
-      $(this).removeClass("focus");
-    }
-  );
+  $(".write__inparea").on("focusin", ".write__inpcnt, .write__txtarea", function() {
+    $(this).addClass("focus");
+  });
+  $(".write__inparea").on("focusout", ".write__inpcnt, .write__txtarea", function() {
+    $(this).removeClass("focus");
+  });
 
   function isAllChecked() {
     var chk = 0;
@@ -155,15 +147,11 @@ $(function() {
   $(".toggle_on_off").on("click", function(e) {
     var group = $(".toggle_on_off");
     var nowIdx = group.index($(this));
-    group.each(function(index, item) {
-      if (index === nowIdx) {
-        $(item).removeClass("off");
-        $(item).addClass("on");
-        return;
-      }
-      $(item).removeClass("on");
-      $(item).addClass("off");
-    });
+    group.addClass("off").removeClass("on");
+    group
+      .eq(nowIdx)
+      .addClass("on")
+      .removeClass("off");
   });
 
   //아코디언탭
@@ -172,25 +160,14 @@ $(function() {
     var hiddenArea = $(this).siblings(".acdo_cont");
     wrapper.toggleClass("on");
   });
+
+  //멘션
+  $("body").on("click", ".detail_replytit", function(e) {
+    var mention = makeMention($(e.target).text());
+    var inputArea = $(".reply_inputctn");
+    !inputArea.children(".reply_mention").length && inputArea.prepend(mention);
+  });
 });
-
-/* 스피너 모음 */
-var _spinner = $(".main-spinner");
-var _target;
-
-function makeSpinner() {
-  return $('<span class="main-spinner"></span>');
-}
-
-function showSpinner(target) {
-  _target = target;
-  _spinner = _spinner.length ? $(_spinner).detach() : makeSpinner();
-  _spinner.appendTo(target);
-}
-
-function hideSpinner() {
-  _spinner = $(_spinner).detach();
-}
 
 function areNotCompleted(group) {
   //모두 입력되었으면 -1리턴
@@ -198,12 +175,6 @@ function areNotCompleted(group) {
     if (!$(group[i]).hasClass("on")) return i;
   }
   return -1;
-}
-
-//토스트
-var toast = $(".toast");
-if (toast.length > 0) {
-  showToast(toast);
 }
 
 function copyToClipboard(url) {
@@ -215,12 +186,23 @@ function copyToClipboard(url) {
   $temp.remove();
 }
 
-function showToast(target) {
-  target.addClass("scene_element--fadein on");
-  target.removeClass("scene_element--fadeout");
+//토스트
+function makeToast(str) {
+  return `<div class="toast scene_element">${str}</div>`;
+}
+
+function showToast(str) {
+  if ($(".toast").length) return false;
+  var toast = $(makeToast(str));
+  $(".wrapper").append(toast);
+  toast.addClass("scene_element--fadein");
+  toast.removeClass("scene_element--fadeout");
   setTimeout(() => {
-    target.removeClass("scene_element--fadein");
-    target.addClass("scene_element--fadeout");
+    toast.removeClass("scene_element--fadein");
+    toast.addClass("scene_element--fadeout");
+    setTimeout(() => {
+      toast.remove();
+    }, 500);
   }, 2000);
 }
 
@@ -232,3 +214,169 @@ var toggleInputBdr = function(target) {
   if (len > 0) target.addClass("on");
   else if (len == 0) target.removeClass("on");
 };
+
+function toggleTab(e) {
+  $(e.delegateTarget)
+    .children()
+    .removeClass("on");
+  $(e.target).addClass("on");
+}
+
+function makeMention(str) {
+  return $(`
+  <div class="reply_mention">${str}
+    <button class="sp00 close" onclick="$(this).parent().remove()"></button>
+  </div>
+  `);
+}
+
+var ID_TMPL_SLIDE = "#slideTmpl";
+var ID_TMPL_REPLY = "#replyTmpl";
+
+var URL_BASE = "http://pickvs.com";
+var URL_CREATE_COMMENT = URL_BASE + "/writeComment";
+var URL_UPDATE_VOTECOUNT = URL_BASE + "/vote";
+var URL_READ_SLIDE_DATA = URL_BASE + "/getDetailDtoList";
+var URL_READ_FIRST_SLIDE_DATA = URL_BASE + "/getWritingDtlDto";
+
+var PAGE_NAME_SWIPE = "swipe";
+
+var ssj = {};
+ssj.util = ssj.util || {};
+
+ssj.util.ajax = function(options) {
+  $.extend(this, options);
+  this.init();
+};
+
+ssj.util.ajax.prototype = {
+  init() {
+    this.assignElements();
+  },
+  assignElements() {
+    this.method = "GET";
+    this.paging = new Map(); // 비동기 요청 종류에 따라 페이지 위치 기억
+  },
+  sendRequest(url, data, tmplId, pageName, method) {
+    this.setMetaData(url, data, tmplId, pageName, method);
+    var oSelf = this;
+    return new Promise(function(resolve, reject) {
+      $.ajax({
+        url: oSelf.url,
+        method: oSelf.method,
+        data: oSelf.data,
+        success: function(data) {
+          console.log(data);
+          oSelf.increasePageNum(pageName);
+          resolve(oSelf.makeHtml(data));
+        },
+        error: function(data) {
+          reject(data);
+        }
+      });
+    });
+  },
+  increasePageNum(pageName) {
+    if (typeof pageName !== "undefined" && !this.isPageEmpty(pageName)) {
+      this.paging.set(pageName, this.getCurrentPageNum(pageName) + 1);
+    }
+  },
+  getCurrentPageNum(pageName) {
+    return !this.isPageEmpty(pageName) ? this.paging.get(pageName) : 1;
+  },
+  isPageEmpty(pageName) {
+    return !this.paging.get(pageName);
+  },
+  setMetaData(url, data, tmplId, pageName, method) {
+    this.setUrl(url);
+    this.setMethod(method);
+    this.setData(data);
+    this.setTmplId(tmplId);
+    if (typeof pageName !== "undefined") {
+      this.activatePaging(pageName);
+    }
+  },
+  setUrl(url) {
+    this.url = url;
+  },
+  setData(data) {
+    this.data = data;
+  },
+  setMethod(method) {
+    this.method = method;
+  },
+  setTmplId(id) {
+    this.ID_TMPL = id;
+  },
+  activatePaging(pageName) {
+    if (this.isPageEmpty(pageName)) {
+      this.paging.set(pageName, 1);
+    }
+  },
+  getTemplate() {
+    return $.templates(this.ID_TMPL);
+  },
+  makeHtml(data) {
+    return this.getTemplate().render(data);
+  }
+};
+
+ssj.util.spinner = function(options) {
+  $.extend(this, options);
+  this.init();
+};
+
+ssj.util.spinner.prototype = {
+  init() {
+    this.initVar();
+  },
+  initVar() {
+    this.spinner = this.makeSpinner();
+  },
+  makeSpinner() {
+    return $('<div class="main-spinner"></div>');
+  },
+  show() {
+    this.spinner.appendTo(this.target);
+  },
+  hide() {
+    this.spinner.detach();
+  },
+  setTarget: target => {
+    this.target = target;
+  }
+};
+
+ssj.util.swiper = function(options) {
+  this.metaData = options;
+  this.init();
+};
+
+ssj.util.swiper.prototype = {
+  init() {
+    this.initVar();
+  },
+  initVar() {
+    this.swiper = new Swiper(".swiper-container", this.metaData);
+  },
+  appendItem(slide) {
+    this.swiper.appendSlide(slide);
+  },
+  getActiveSlide() {
+    return this.swiper.slides[this.getActiveIndex()];
+  },
+  getActiveIndex() {
+    return this.swiper.activeIndex;
+  },
+  getTotalCount() {
+    return this.swiper.slides.length;
+  }
+};
+
+function getNumberInStr(str) {
+  return str.replace(/[^0-9]/g, "");
+}
+
+function scrollToBottom(target) {
+  $(target).animate({ scrollTop: $(document).height() }, 0);
+}
